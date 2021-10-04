d525e0904cdc54a40e4b01e2e5d21f063334cf77
Muhamad Reksy Mulia <rreksy@untan.ac.id>
2021-10-04 12:27:17 +0700

    remove redundant primary key declaration

--- app/user/model.go
+++ app/user/model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type User struct {
-	ID        uint   `gorm:"primaryKey; not null;uniqueIndex;primary_key"`
+	ID        uint   `gorm:"primaryKey; not null;uniqueIndex;"`
 	Name      string `gorm:"size:255"`
 	Email     string `gorm:"size:255"`
 	Password  string `gorm:"size:255"`
