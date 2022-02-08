43312478f4682c7eef9c009feca4e71fa85ef207
Muhamad Reksy Mulia <rreksy@untan.ac.id>
2022-02-08 15:13:00 +0700

    modify error message and add Pretty Struct to print in console

--- app/helper/helper.go
+++ app/helper/helper.go
@@ -1,6 +1,11 @@
 package helper
 
-import "github.com/go-playground/validator/v10"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type Response struct {
 	Meta Meta        `json:"meta"`
@@ -31,9 +36,37 @@ func APIResponse(message string, code int, status string, data interface{}) Resp
 func FormatValidationError(err error) []string {
 	var errors []string
 
-	for _, e := range err.(validator.ValidationErrors) {
-		errors = append(errors, e.Error())
+	if castedObject, ok := err.(validator.ValidationErrors); ok {
+		var errorsMessage string
+		for _, err := range castedObject {
+			switch err.Tag() {
+			case "required":
+				errorsMessage = fmt.Sprintf("%s is required",
+					err.Field())
+			case "email":
+				errorsMessage = fmt.Sprintf("%s is not valid email",
+					err.Field())
+			case "gte":
+				errorsMessage = fmt.Sprintf("%s value must be greater than %s",
+					err.Field(), err.Param())
+			case "lte":
+				errorsMessage = fmt.Sprintf("%s value must be lower than %s",
+					err.Field(), err.Param())
+			}
+			errors = append(errors, errorsMessage)
+			// break
+		}
 	}
 
 	return errors
+}
+
+func PrettyStruct(data interface{}) (string, error) {
+	val, err := json.MarshalIndent(data, "", "    ")
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(val), nil
 }
